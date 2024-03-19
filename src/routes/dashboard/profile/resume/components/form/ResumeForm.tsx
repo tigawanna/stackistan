a70ea4d1c10cdb9c1f7048581f627bdabadad741
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-03-19 21:06:12 +0300

    add form features

--- src/routes/dashboard/profile/resume/components/form/ResumeForm.tsx
+++ src/routes/dashboard/profile/resume/components/form/ResumeForm.tsx
@@ -4,11 +4,18 @@ import { StackistanResumeProfileCreate } from "@/lib/pb/database";
 import { useViewer } from "@/lib/pb/hooks/useViewer";
 import { SelectResumeProfileType } from "./SelectResumeProfileType";
 import ClientSuspenseWrapper from "@/components/wrappers/ClientSuspenseWrapper";
-import { ClientSuspense } from "rakkasjs";
+import { ClientSuspense, usePageContext } from "rakkasjs";
+import { useMutation } from "@tanstack/react-query";
+import { pbTryCatchWrapper } from "@/lib/pb/utils";
+import { ArrayFormFields } from "./ArrayFormFields";
+import { StackistanResumeProfileEducation } from "@/lib/pb/custom-db-types";
 
 interface ResumeFormProps {}
 
 export function ResumeForm({}: ResumeFormProps) {
+  const {
+    locals: { pb },
+  } = usePageContext();
   const {
     data: { user },
   } = useViewer();
@@ -18,7 +25,9 @@ export function ResumeForm({}: ResumeFormProps) {
         user: user?.record.id!,
         type: "general",
         projects: {
-          list: [],
+          list: [
+            { name: "name 1", description: "description 1", link: "link 1" },
+          ],
         },
         skills: {
           list: [],
@@ -35,10 +44,19 @@ export function ResumeForm({}: ResumeFormProps) {
         },
       },
     });
+
+  const mutation = useMutation({
+    mutationFn: ({ inp }: { inp: StackistanResumeProfileCreate }) => {
+      return pbTryCatchWrapper(
+        pb.from("stackistan_resume_profile").create(inp),
+      );
+    },
+  });
+  const pb_error = mutation?.data?.error;
   return (
     <div className="w-full h-full flex flex-col items-center justify-center pl-3">
       {/*  intro */}
-      <div className="w-full flex-col px-2 pt-10">
+      <div className="w-full h-fit flex-col px-2 pt-10">
         <h1 className="text-3xl fonr bold">Create resume profile</h1>
         <p className="text-sm brightness-75 md:max-w-[70%]">
           Create your resume profile to use in future job applications , the
@@ -46,17 +64,111 @@ export function ResumeForm({}: ResumeFormProps) {
           one for backead frontend developer if you are fullstack
         </p>
       </div>
-      <form className="h-full w-full flex flex-col justify-center items-center">
+      <form className="h-full w-full flex flex-col justify-center items-center overflow-auto ">
         {/* select resume profile type */}
-        <ClientSuspense fallback={<div className="h-8 bg-base-300 skeleton"></div>}>
-        <ClientSuspenseWrapper>
-        <PBFieldWrapper field_key={"type"}>
-          <SelectResumeProfileType />
-        </PBFieldWrapper>
-          
-        </ClientSuspenseWrapper>
-          
+        <ClientSuspense
+          fallback={<div className="h-8 bg-base-300 skeleton"></div>}
+        >
+          <ClientSuspenseWrapper>
+            <PBFieldWrapper field_key={"type"} pb_error={pb_error}>
+              <SelectResumeProfileType
+                resume_type={input.type}
+                setResumeType={(type) =>
+                  setInput((prev) => {
+                    return {
+                      ...prev,
+                      type,
+                    };
+                  })
+                }
+              />
+            </PBFieldWrapper>
+          </ClientSuspenseWrapper>
         </ClientSuspense>
+        {/* projects */}
+        <div className="w-full">
+          <span className="text-2xl font-bold">Projects</span>
+          <ArrayFormFields
+            items_shape={{
+              name: "",
+              description: "",
+              link: "",
+            }}
+            items={input.projects}
+            setArrayValues={(value) => {
+              setInput((prev) => {
+                return {
+                  ...prev,
+                  projects: { list: value },
+                };
+              });
+            }}
+          />
+        </div>
+
+        {/* skills */}
+        <div className="w-full">
+          <span className="text-2xl font-bold">Skills</span>
+          <ArrayFormFields
+            items_shape={{
+              name: "",
+              level: 0,
+            }}
+            items={input.skills}
+            setArrayValues={(value) => {
+              setInput((prev) => {
+                return {
+                  ...prev,
+                  skills: { list: value },
+                };
+              });
+            }}
+          />
+        </div>
+
+        {/* activities */}
+        <div className="w-full">
+          <span className="text-2xl font-bold">Activities</span>
+          <ArrayFormFields
+            items_shape={{
+              name: "",
+              description: "",
+              link: "",
+            }}
+            items={input.activities}
+            setArrayValues={(value) => {
+              setInput((prev) => {
+                return {
+                  ...prev,
+                  activities: { list: value },
+                };
+              });
+            }}
+          />
+        </div>
+
+        {/* education */}
+        <div className="w-full">
+          <span className="text-2xl font-bold">Education</span>
+          <ArrayFormFields<StackistanResumeProfileEducation>
+            items_shape={{
+              school: "",
+              fieldOfStudy: "",
+              qualification: "Certificate",
+              from: "",
+              to: "",
+            }}
+            items={input.education}
+            setArrayValues={(value) => {
+              setInput((prev) => {
+                return {
+                  ...prev,
+                  education: { list: value },
+                };
+              });
+            }}
+          />
+        </div>
       </form>
     </div>
   );

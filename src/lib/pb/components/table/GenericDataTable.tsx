3435a1d28a08b4bcf1264e7c838f7be7c5e03a28
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-03-27 18:10:14 +0300

    add row sekection

--- src/lib/pb/components/table/GenericDataTable.tsx
+++ src/lib/pb/components/table/GenericDataTable.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FieldType } from "./types";
 import { ChevronDown, ChevronUp } from "lucide-react";
+import { Checkbox } from "@/components/shadcn/ui/checkbox";
 
 type TableColumns<T extends Record<string, any>> = {
   [K in keyof T]?: {
@@ -19,6 +20,8 @@ export function GenericDataTable<T extends Record<string, any>>({
   columns,
 }: GenericDataTableProps<T>) {
   const [tableRows, setTableRows] = useState(list);
+  const [fliterBy, setFilterBy] = useState<keyof T | null>(null);
+  const [selectedRows, setSelectedRows] = useState<string[]>([]);
 
   function sortBy(key: keyof T, direction: "asc" | "desc") {
     setTableRows((prev) => {
@@ -33,11 +36,43 @@ export function GenericDataTable<T extends Record<string, any>>({
       });
     });
   }
+  function selectRow(id: string) {
+    if (selectedRows.includes(id)) {
+      setSelectedRows((prev) => {
+        if (prev) {
+          return prev.filter((i) => i !== id);
+        }
+        return prev;
+      });
+    } else {
+      setSelectedRows((prev) => {
+        if (!prev) {
+          return prev;
+        }
+        return [...prev, id];
+      });
+    }
+  }
+  function selectAllRows(checked: boolean) {
+    console.log({ checked });
+    if (checked) {
+      setSelectedRows(list.map((i) => i.id));
+    } else {
+      setSelectedRows([]);
+    }
+  }
   return (
     <div className="w-full h-full  p-5">
       <table className="w-fit table bg-base-300/40 ">
         <thead className="w-full bg-base-300 sticky top-0">
           <tr className="w-full text-lg">
+            <th className="">
+              <Checkbox
+                checked={selectedRows.length === list.length}
+                onCheckedChange={selectAllRows}
+              />
+            </th>
+
             {Object.entries(columns).map(([key, value]) => {
               return (
                 <th className="" key={key}>
@@ -60,15 +95,23 @@ export function GenericDataTable<T extends Record<string, any>>({
           </tr>
         </thead>
         <tbody>
-          {tableRows.map((item) => (
-            <tr key={item.id}>
+          {tableRows.map((item) => {
+            const checked = selectedRows.includes(item.id);
+            return (
+            <tr key={item.id} className={checked ? "bg-primary/10" : ""}>
+              <td>
+                <Checkbox
+                  checked={checked}
+                  onCheckedChange={() => selectRow(item.id)}
+                />
+              </td>
               {Object.entries(columns).map(([key, value]) => {
                 if (value?.fieldKey) {
                   return <td key={item.id + key}>{item[value?.fieldKey]}</td>;
                 }
               })}
             </tr>
-          ))}
+          )})}
         </tbody>
       </table>
     </div>

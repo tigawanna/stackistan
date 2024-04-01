c185129230962061160d86475bb752f18ebec71e
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-04-01 17:06:09 +0300

    add job applcation page

---
+++ src/routes/dashboard/job/index.page.tsx
@@ -0,0 +1,7 @@
+import { PageProps } from "rakkasjs"
+export default function JobApplicationPage({}:PageProps) {
+return (
+<div className="w-full h-full  flex flex-col items-center justify-center">
+    Job Application
+</div>
+)}
